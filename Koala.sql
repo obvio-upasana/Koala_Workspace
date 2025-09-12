fb6e689ba0f8d1630190621f94438fe0c440a839
Upasana <officialupasana75@gmail.com>
2025-09-12 12:03:31 +0530

    Update Koala.sql

--- Koala.sql
+++ Koala.sql
@@ -65,6 +65,26 @@ CREATE TABLE public."Computers" (
 ALTER TABLE public."Computers" OWNER TO postgres;
 
 --
+-- Name: Customers; Type: TABLE; Schema: public; Owner: postgres
+--
+
+CREATE TABLE public."Customers" (
+    membership_id bigint NOT NULL,
+    first_name character varying NOT NULL,
+    last_name character varying NOT NULL,
+    phone bigint NOT NULL,
+    email character varying NOT NULL,
+    "Address" text NOT NULL,
+    "Start_date" date NOT NULL,
+    "End_date" date NOT NULL,
+    payment_id character varying NOT NULL,
+    "Status" boolean NOT NULL
+);
+
+
+ALTER TABLE public."Customers" OWNER TO postgres;
+
+--
 -- Name: Inventory; Type: TABLE; Schema: public; Owner: postgres
 --
 
@@ -80,6 +100,50 @@ CREATE TABLE public."Inventory" (
 
 
 ALTER TABLE public."Inventory" OWNER TO postgres;
+
+--
+-- Name: Memebership; Type: TABLE; Schema: public; Owner: postgres
+--
+
+CREATE TABLE public."Memebership" (
+    membership_id bigint NOT NULL,
+    member_name text NOT NULL,
+    start_date date NOT NULL,
+    end_date date NOT NULL,
+    status boolean NOT NULL
+);
+
+
+ALTER TABLE public."Memebership" OWNER TO postgres;
+
+--
+-- Name: Payment_methods; Type: TABLE; Schema: public; Owner: postgres
+--
+
+CREATE TABLE public."Payment_methods" (
+    payment_id character varying(10) NOT NULL,
+    payment_method_id text NOT NULL,
+    last_four integer NOT NULL,
+    payment_token character varying NOT NULL
+);
+
+
+ALTER TABLE public."Payment_methods" OWNER TO postgres;
+
+--
+-- Name: Payments; Type: TABLE; Schema: public; Owner: postgres
+--
+
+CREATE TABLE public."Payments" (
+    payment_id character varying NOT NULL,
+    membership_id bigint NOT NULL,
+    amount integer NOT NULL,
+    payment_date date NOT NULL,
+    "Description" text
+);
+
+
+ALTER TABLE public."Payments" OWNER TO postgres;
 
 --
 -- Name: Rooms; Type: TABLE; Schema: public; Owner: postgres
@@ -148,10 +212,42 @@ COPY public."Computers" (computer_id, status, last_used) FROM stdin;
 
 
 --
+-- Data for Name: Customers; Type: TABLE DATA; Schema: public; Owner: postgres
+--
+
+COPY public."Customers" (membership_id, first_name, last_name, phone, email, "Address", "Start_date", "End_date", payment_id, "Status") FROM stdin;
+\.
+
+
+--
 -- Data for Name: Inventory; Type: TABLE DATA; Schema: public; Owner: postgres
 --
 
 COPY public."Inventory" (item_id, item_name, quantity_on_hand, unit_of_measure, restock_threshold, last_restocked_date, "Storagechange_bill_id") FROM stdin;
+\.
+
+
+--
+-- Data for Name: Memebership; Type: TABLE DATA; Schema: public; Owner: postgres
+--
+
+COPY public."Memebership" (membership_id, member_name, start_date, end_date, status) FROM stdin;
+\.
+
+
+--
+-- Data for Name: Payment_methods; Type: TABLE DATA; Schema: public; Owner: postgres
+--
+
+COPY public."Payment_methods" (payment_id, payment_method_id, last_four, payment_token) FROM stdin;
+\.
+
+
+--
+-- Data for Name: Payments; Type: TABLE DATA; Schema: public; Owner: postgres
+--
+
+COPY public."Payments" (payment_id, membership_id, amount, payment_date, "Description") FROM stdin;
 \.
 
 
@@ -203,11 +299,43 @@ ALTER TABLE ONLY public."Computers"
 
 
 --
+-- Name: Customers Customers_pkey; Type: CONSTRAINT; Schema: public; Owner: postgres
+--
+
+ALTER TABLE ONLY public."Customers"
+    ADD CONSTRAINT "Customers_pkey" PRIMARY KEY (membership_id, phone);
+
+
+--
 -- Name: Inventory Inventory_pkey; Type: CONSTRAINT; Schema: public; Owner: postgres
 --
 
 ALTER TABLE ONLY public."Inventory"
     ADD CONSTRAINT "Inventory_pkey" PRIMARY KEY (item_id);
+
+
+--
+-- Name: Memebership Memebership_pkey; Type: CONSTRAINT; Schema: public; Owner: postgres
+--
+
+ALTER TABLE ONLY public."Memebership"
+    ADD CONSTRAINT "Memebership_pkey" PRIMARY KEY (membership_id);
+
+
+--
+-- Name: Payment_methods Payment_methods_pkey; Type: CONSTRAINT; Schema: public; Owner: postgres
+--
+
+ALTER TABLE ONLY public."Payment_methods"
+    ADD CONSTRAINT "Payment_methods_pkey" PRIMARY KEY (payment_id);
+
+
+--
+-- Name: Payments Payments_pkey; Type: CONSTRAINT; Schema: public; Owner: postgres
+--
+
+ALTER TABLE ONLY public."Payments"
+    ADD CONSTRAINT "Payments_pkey" PRIMARY KEY (payment_id);
 
 
 --
@@ -227,11 +355,43 @@ ALTER TABLE ONLY public."Staff"
 
 
 --
+-- Name: Payments Membership_id_fkey; Type: FK CONSTRAINT; Schema: public; Owner: postgres
+--
+
+ALTER TABLE ONLY public."Payments"
+    ADD CONSTRAINT "Membership_id_fkey" FOREIGN KEY (membership_id) REFERENCES public."Memebership"(membership_id);
+
+
+--
+-- Name: Payment_methods Payment_fkey; Type: FK CONSTRAINT; Schema: public; Owner: postgres
+--
+
+ALTER TABLE ONLY public."Payment_methods"
+    ADD CONSTRAINT "Payment_fkey" FOREIGN KEY (payment_id) REFERENCES public."Payments"(payment_id);
+
+
+--
+-- Name: Customers Payment_id; Type: FK CONSTRAINT; Schema: public; Owner: postgres
+--
+
+ALTER TABLE ONLY public."Customers"
+    ADD CONSTRAINT "Payment_id" FOREIGN KEY (payment_id) REFERENCES public."Payments"(payment_id);
+
+
+--
 -- Name: Attendance Staff_ID_fkey; Type: FK CONSTRAINT; Schema: public; Owner: postgres
 --
 
 ALTER TABLE ONLY public."Attendance"
     ADD CONSTRAINT "Staff_ID_fkey" FOREIGN KEY ("Staff_id") REFERENCES public."Staff"("Staff_id") ON DELETE RESTRICT;
+
+
+--
+-- Name: Customers member_fkey; Type: FK CONSTRAINT; Schema: public; Owner: postgres
+--
+
+ALTER TABLE ONLY public."Customers"
+    ADD CONSTRAINT member_fkey FOREIGN KEY (membership_id) REFERENCES public."Memebership"(membership_id);
 
 
 --
